7adbfa4bb0141e435b4b7d7e1e28833dd85fdd64
Mazlum <mazlumsert@gmail.com>
2017-05-25 22:43:28 +0200

    3 methods has been tested using mocks

--- src/test/java/io/BookCtrlTest.java
+++ src/test/java/io/BookCtrlTest.java
@@ -1,15 +1,21 @@
 package io;
 
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-import static org.hamcrest.CoreMatchers.is;
-
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
+
 
 /**
  * Created by mazlumsert on 25/05/2017.
@@ -30,11 +36,33 @@ public class BookCtrlTest {
 
     @Test
     public void testIfCityListIsEmpty() throws IOException {
-        assertThat(book.getCities().isEmpty(), is(false)  );
+        assertThat(book.getCities().isEmpty(), is(false));
+    }
+
+
+    @Test
+    public void testGetTitle(){
+      List<String> mockTitleList = new ArrayList<>();
+        mockTitleList.add("he Rime of the Ancient Mariner");
+        mockTitleList.add("Lincolns Second Inaugural Address, March 4, 1865");
+        mockTitleList.add("Peter Pan Peter Pan and Wendy");
+
+        BookCtrl book1 = mock(BookCtrl.class);
+        when(book1.getTitles(false)).thenReturn(mockTitleList);
     }
 
     @Test
-    public void testGetTitles(){
-        assertThat(book.getTitles());
+    public void testGetAuthor(){
+      List<String> mockAuthorList = mock(List.class);
+      when(mockAuthorList.get(0)).thenReturn("Samuel Taylor Coleridge 1");
+      when(mockAuthorList.get(1)).thenReturn("Abraham Lincoln");
+      when(mockAuthorList.get(2)).thenReturn("James M. Barrie");
+
+      BookCtrl book2 = mock(BookCtrl.class);
+      when(book2.getAuthors(false)).thenReturn(mockAuthorList);
     }
+
+
+
+
 }
