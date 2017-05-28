4b72c31aef2b21b2a94b30bb11ffb264b0378b6b
bigstepdenmark <i@bigstep.dk>
2017-05-28 18:39:54 +0200

    MySQLTest i gang..6

--- src/test/java/database/MySQLTest.java
+++ src/test/java/database/MySQLTest.java
@@ -1,78 +1,121 @@
 package database;
 
+import org.dbunit.*;
+import org.dbunit.database.IDatabaseConnection;
+import org.dbunit.dataset.CompositeDataSet;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.ITable;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.dbunit.operation.DatabaseOperation;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
+import java.io.File;
+import java.sql.Connection;
 import java.sql.DriverManager;
 
 import static org.junit.Assert.*;
 import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by ismailcam on 27/05/2017.
  */
-public class MySQLTest
+//@RunWith( MockitoJUnitRunner.class ) // Enable Mockito Annotations
+public class MySQLTest //extends DBTestCase
 {
+
+//    IDatabaseConnection connection;
+//    IDataSet dataSet;
+
+    //@Mock
     MySQL mySQL;
+
+/*    public MySQLTest(String name)
+    {
+        super( name );
+        System.setProperty( PropertiesBasedJdbcDatabaseTester.DBUNIT_DRIVER_CLASS, "com.mysql.jdbc.Driver" );
+        System.setProperty( PropertiesBasedJdbcDatabaseTester.DBUNIT_CONNECTION_URL,
+                            "jdbc:mysql://127.0.0.1:3306/gutenberg" );
+        System.setProperty( PropertiesBasedJdbcDatabaseTester.DBUNIT_USERNAME, "root" );
+        System.setProperty( PropertiesBasedJdbcDatabaseTester.DBUNIT_PASSWORD, "root" );
+        System.setProperty( PropertiesBasedJdbcDatabaseTester.DBUNIT_SCHEMA, "gutenberg" );
+    }
+
+    @Override
+    protected IDataSet getDataSet() throws Exception
+    {
+        IDataSet[] datasets = new IDataSet[]{
+                new FlatXmlDataSetBuilder().build( new File( new File(
+                        "src/main/files/databaseExports/partial/books.xml" ).toURI().getPath() ) ),
+                new FlatXmlDataSetBuilder().build( new File( new File(
+                        "src/main/files/databaseExports/partial/authors.xml" ).toURI().getPath() ) ),
+                new FlatXmlDataSetBuilder().build( new File( new File(
+                        "src/main/files/databaseExports/partial/author_books.xml" ).toURI().getPath() ) ),
+                new FlatXmlDataSetBuilder().build( new File( new File(
+                        "src/main/files/databaseExports/partial/cities.xml" ).toURI().getPath() ) ),
+                new FlatXmlDataSetBuilder().build( new File( new File(
+                        "src/main/files/databaseExports/partial/book_cities.xml" ).toURI().getPath() ) )
+        };
+
+        return new CompositeDataSet( datasets );
+
+    }*/
+
+    protected DatabaseOperation getSetUpOperation() throws Exception
+    {
+        return DatabaseOperation.REFRESH;
+    }
+
+    protected DatabaseOperation getTearDownOperation() throws Exception
+    {
+        return DatabaseOperation.NONE;
+    }
 
     @Before
     public void setUp() throws Exception
     {
-        mySQL = new MySQL( DriverManager.getConnection( "jdbc:mysql://127.0.0.1:3306/gutenberg", "root", "root" ) );
+        mySQL = new MySQL();
+//        connection = this.getConnection();
+//        dataSet = getDataSet();
+        //super.setUp();
     }
 
     @After
     public void tearDown() throws Exception
     {
+       // super.tearDown();
     }
 
     @Test
-    public void connect() throws Exception
+    public void testTableCount() throws Exception
     {
+      //  String query = "SELECT * FROM books where id = 10";
+
+        //System.out.println( connection.createDataSet().getTableNames().length );
+
+        //assertThat( connection, is( "" ) );
+
+        //assertThat( title, is( "The King James Bible" ) );
+
+        assertThat( true, is( true ) );
     }
 
-    @Test
-    public void close() throws Exception
+    /*    @Test
+    public void testMe() throws Exception
     {
-    }
+        IDataSet databaseDataSet = getConnection().createDataSet();
+        ITable actualTable = databaseDataSet.getTable( "books" );
 
-    @Test
-    public void booksByCity() throws Exception
-    {
-    }
+        ITable expectedTable = databaseTester.getDataSet().getTable( "books" );
 
-    @Test
-    public void booksByTitle() throws Exception
-    {
-    }
+        Assertion.assertEquals( expectedTable, actualTable );
 
-    @Test
-    public void booksByAuthor() throws Exception
-    {
-    }
+    }*/
 
-    @Test
-    public void booksByLocation() throws Exception
-    {
-    }
-
-    @Test
-    public void getBookById() throws Exception
-    {
-        String title = mySQL.getBookById( 10 ).getTitle();
-
-        assertThat( title, is( "The King James Bible" ) );
-    }
-
-    @Test
-    public void getBooks() throws Exception
-    {
-    }
-
-    @Test
-    public void addCitiesToBooks() throws Exception
-    {
-    }
 
 }
